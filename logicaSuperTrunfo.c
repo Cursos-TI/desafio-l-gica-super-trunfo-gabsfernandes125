39f9b1c08d97176f7ea07fd6f90909740e07d457
gabsfernandes125 <gbeenson@gmail.com>
2025-08-06 01:35:46 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,9 +2,9 @@
 int main (){
 
     //organização e declaração de variáveis
-int codigo1, codigo2, npontos1, npontos2;
+int opcaofinal, codigo1, codigo2, npontos1, npontos2;
 char carta1[2], carta2[2], estado1[3], estado2[3], cidade1[50], cidade2[50]; 
-float pop1, pop2, area1, area2, PIB1, PIB2, densidade1, densidade2, PPC1, PPC2;
+float pop1, pop2, area1, area2, PIB1, PIB2, densidade1, densidade2;
 
 printf ("\n\n======================================");
 printf ("\n====== BEM-VINDO AO SUPER TRUNFO ======");
@@ -14,29 +14,29 @@ printf ("\n======================================\n\n");
     //primeira sessão de entradas - CARTA 1.
 
 printf (">>>>>>>>>> CARTA 1 <<<<<<<<<\n");
-printf ("Carta (A ou B): ");
-scanf ("%1s", carta1);
+    printf ("Carta: ");
+    scanf ("%1s", carta1);
 
 printf ("Estado: ");
-scanf ("%2s", estado1);
+    scanf ("%2s", estado1);
 
 printf ("Codigo: %2s", estado1);
-scanf ("%d", &codigo1);
+    scanf ("%d", &codigo1);
 
 printf ("Nome da cidade: ");
-scanf ("%49s", cidade1);
+    scanf ("%49s", cidade1);
 
 printf ("Populacao: ");
-scanf ("%f", &pop1);
+    scanf ("%f", &pop1);
 
 printf ("Area (em M quadrado): ");
-scanf ("%f", &area1);
+        scanf ("%f", &area1);
 
 printf ("PIB: ");
-scanf ("%f", &PIB1);
+    scanf ("%f", &PIB1);
 
 printf ("Numeros de pontos turisticos: ");
-scanf ("%d", &npontos1);
+    scanf ("%d", &npontos1);
 
 printf ("Densidade populacional: %f\n", pop1/area1);
 
@@ -45,48 +45,108 @@ printf ("PIB per capita: %f", PIB1/pop1);
 
 //segunda sessão de entrdas - CARTA 2.
 
-printf ("\n\n"); //preferi dar espaços assim por questão estética rsrsrs
-printf (">>>>>>>>>> CARTA 2 <<<<<<<<<\n");
-printf ("Carta (A ou B): ");
-scanf ("%1s", carta2);
+printf ("\n\n>>>>>>>>>> CARTA 2 <<<<<<<<<\n");
+    printf ("Carta: ");
+    scanf ("%1s", carta2);
 
 printf ("Estado: ");
-scanf ("%2s", estado2);
+    scanf ("%2s", estado2);
 
 printf ("Codigo: %2s", estado2);
-scanf ("%d", &codigo2);
+    scanf ("%d", &codigo2);
 
 printf ("Nome da cidade: ");
-scanf ("%49s", cidade2);
+    scanf ("%49s", cidade2);
 
 printf ("Populacao: ");
-scanf ("%f", &pop2);
+    scanf ("%f", &pop2);
 
 printf ("Area (em M quadrado): ");
-scanf ("%f", &area2);
+    scanf ("%f", &area2);
 
 printf ("PIB: ");
-scanf ("%f", &PIB2);
+    scanf ("%f", &PIB2);
 
 printf ("Numeros de pontos turisticos: ");
-scanf ("%d", &npontos2);
+    scanf ("%d", &npontos2);
 
-printf ("Densidade populacional: %f\n", pop2/area2);
+do {
+printf ("\n\n\n>>>>> AGORA COMPARE OS VALORES E VEJA QUEM GANHA: <<<<<<\n");
+    printf ("1 - POPULACOES\n");
+        printf ("2 - AREA\n");
+            printf ("3 - PIB\n");
+                printf ("4 - NUMERO DE PONTOS TURISTICOS\n");
+                    printf ("5 - DENSIDADE POPULACIONAL\n");
+                        printf ("6 - PIB PER CAPITA\n");
+scanf ("%d", &opcaofinal);
 
-printf ("PIB per capita: %f\n\n", PIB2/pop2);
+switch (opcaofinal){//CHAVE DE ABERTURA DO SWITCH
+    case 1:
+        if (pop1 >= pop2){
+            printf ("---> ENTRE POPULACOES: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+            printf ("---> ENTRE POPULACOES: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;
 
-printf ("Carta1 - %s(%2s): %f\n", cidade1, estado1, pop1);
-printf ("Carta2 - %s(%2s): %f\n\n", cidade2, estado2, pop2);
+    case 2:
+        if (area1 >= area2){
+            printf ("---> ENTRE AREAS: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+            printf ("---> ENTRE AREAS: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;
 
-if (pop1 > pop2) {
-    printf ("%s ganhou!!!!\n", cidade1);
+    case 3: 
+        if (PIB1 >= PIB2){
+            printf ("---> ENTRE PIB's: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+            printf ("---> ENTRE PIB's: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;
+
+    case 4:
+        if (npontos1 >= npontos2){
+            printf ("---> ENTRE PONTOS TURISTICOS: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+            printf ("---> ENTRE PONTOS TURISTICOS: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;    
+    
+    case 5:
+        if (densidade1/pop1 >= densidade2/pop2){
+            printf ("---> ENTRE DENSIDADES: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+            printf ("---> ENTRE DENSIDADES: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;
+
+
+    case 6:
+        if (PIB1/pop1 >= PIB2/pop2){
+        printf ("---> ENTRE PIB's PER CAPITA: %s(%s) VENCE <---", cidade1, estado1);
+        }
+        else {
+        printf ("---> ENTRE PIB's PER CAPITA: %s(%s) VENCE <---", cidade2, estado2);
+        }
+        break;
+
+    default: {
+        printf ("OPCAO INVALIDA");
+    }
+
+    }//CHAVE DE FECHAMENTO DO SWITCH
+
 }
-else {
-    printf ("RESULTADO: %s ganhou!!!", cidade2);
-}
+    while (opcaofinal <= 6);
+        printf ("\n\n\nSAINDO...\n\n\n");
 
 printf ("\n\n\n>>>>>>>>>>>>>>>>>>>>>>>>> FIMMMMMMMMMMM :-) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-//LINK DO GITHUB
-//https://github.com/Cursos-TI/desafio-cadastro-das-cartas-no-super-trunfo-gabsfernandes125.git
-    return 0;
+
+return 0;
 }
